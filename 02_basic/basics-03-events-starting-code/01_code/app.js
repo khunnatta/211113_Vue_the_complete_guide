b0ac7bcac3da1a2cf0b312648f57b7c5ace4c061
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-15 19:26:02 +0700

    basic03 add modifier and event to html

--- 02_basic/basics-03-events-starting-code/01_code/app.js
+++ 02_basic/basics-03-events-starting-code/01_code/app.js
@@ -2,25 +2,34 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
     setNameA(event) {
       // event.target is vanilla Js to point the target to html element.
       // using event.target.value = get value from targeted html elements.
-      this.name = event.target.value 
+      this.name = event.target.value;
     },
     setNameB(event, lastName) {
-      this.name = event.target.value + ' ' + lastName; 
+      this.name = event.target.value + " " + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+    },
+    submitForm(event) {
+      //using event.preventDefault() is not to submit the form, it is vanilla Js event function
+      event.preventDefault();
+      alert('submit')
+    },
+    confirmInput(event) {
+      this.confirmedName = event.target.value
     }
-  }
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
