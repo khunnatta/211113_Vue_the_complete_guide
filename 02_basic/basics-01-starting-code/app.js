d3073db94891ff3ccdb7206d15f03a9209ccf6b0
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-15 15:01:11 +0700

    add recalling method into basic01"

--- 02_basic/basics-01-starting-code/app.js
+++ 02_basic/basics-01-starting-code/app.js
@@ -1,4 +1,5 @@
 // vue will control and element by passing object through this function
+// In createApp data is function & methods is object (dict)
 const app = Vue.createApp({
     /*data: function {} */ // the full version
     data() {
@@ -6,11 +7,20 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course & learn Vue!',
             vueLink: 'https://www.google.com/'
         };
-    } // the short hand, will return only object
+    }, // the short hand, will return only object
+    methods: { //method is use to set function to run by some command
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master'
+            }
+        }
+    },
 }); 
 
-/*
-app.mount() is used to link vue to html element link
+/*app.mount() is used to link vue to html element link
 Element that link by vue, 
 the child element will be controlled by vue as well
 */ 
