fe2874f37a2f297619954a4c0f9c256c669c360f
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-15 18:35:46 +0700

    basic03 add click event with multiple arguments using $event

--- 02_basic/basics-03-events-starting-code/01_code/app.js
+++ 02_basic/basics-03-events-starting-code/01_code/app.js
@@ -2,14 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
-    add() {
-      return this.counter = this.counter + 1;
+    setNameA(event) {
+      // event.target is vanilla Js to point the target to html element.
+      // using event.target.value = get value from targeted html elements.
+      this.name = event.target.value 
     },
-    reduce() {
-      return this.counter --
+    setNameB(event, lastName) {
+      this.name = event.target.value + ' ' + lastName; 
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
     }
   }
 });
