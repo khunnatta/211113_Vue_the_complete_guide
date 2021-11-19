834ec6db5eaff73d559ce623296427c00eb55c9f
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-19 18:53:20 +0700

    add dynamic style class

--- 00_assignment/03_assignment_3_reactivity/app.js
+++ 00_assignment/03_assignment_3_reactivity/app.js
@@ -1,37 +1,35 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            result: 0,
-        }
+  data() {
+    return {
+      result: 0,
+    };
+  },
+  methods: {
+    add5() {
+      this.result += 5;
+      console.log(this.result);
     },
-    methods: {
-        add5() {
-            this.result += 5;
-            console.log(this.result);
-        },
-        add1() {
-            this.result += 1;
-        }
+    add1() {
+      this.result += 1;
     },
-    computed: {
-        showResult() {
-            if (this.result < 37) {
-                return "Not there yet";
-            }
-            else if (this.result > 37) {
-                return "Too much!";
-            }
-        }
+  },
+  computed: {
+    showResult() {
+      if (this.result < 37) {
+        return "Not there yet";
+      } else if (this.result > 37) {
+        return "Too much!";
+      }
     },
-    watch: {
-        result(value) {
-            const that = this;
-            setTimeout(function () {
-                that.result = 0;
-            }, 5000);
+  },
+  watch: {
+    result(value) {
+      const that = this;
+      setTimeout(function () {
+        that.result = 0;
+      }, 5000);
+    },
+  },
+});
 
-        }
-    }
-})
-
-app.mount('#assignment')
+app.mount("#assignment");
