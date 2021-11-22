0b913146db40e88491db2cf1055f056c9788f1e2
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-22 20:06:56 +0700

    add all method for css dynamic style

--- 02_basic/basics-10-styling-starting-setup/app.js
+++ 02_basic/basics-10-styling-starting-setup/app.js
@@ -1,22 +1,28 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
-    };
-  },
-  methods: {
-    boxSelected(box) {
-      if (box === "A") {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === "B") {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === "C") {
-        this.boxCSelected = !this.boxCSelected;
-      }
-    },
-  },
+	data() {
+		return {
+			boxASelected: false,
+			boxBSelected: false,
+			boxCSelected: false,
+		};
+	},
+	methods: {
+		boxSelected(box) {
+			if (box === "A") {
+				this.boxASelected = !this.boxASelected;
+			} else if (box === "B") {
+				this.boxBSelected = !this.boxBSelected;
+			} else if (box === "C") {
+				this.boxCSelected = !this.boxCSelected;
+			}
+		},
+	},
+	computed: {
+		boxAClasses() {
+			console.log("use computed method");
+			return { active: this.boxASelected };
+		},
+	},
 });
 
 app.mount("#styling");
