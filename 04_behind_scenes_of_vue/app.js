d72095d3248daf7b57b2b53ef62641aa4ead27ba
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-24 23:12:08 +0700

    add template into 04_behind_scene

--- 04_behind_scenes_of_vue/app.js
+++ 04_behind_scenes_of_vue/app.js
@@ -1,18 +1,31 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    data() {
+        return {
+            currentUserInput: "",
+            message: "Vue is great!",
+        };
     },
-    setText() {
-      this.message = this.currentUserInput;
+    methods: {
+        saveInput(event) {
+            this.currentUserInput = event.target.value;
+        },
+        setText() {
+            this.message = this.currentUserInput;
+        },
     },
-  },
 });
 
-app.mount('#app');
+const app2 = Vue.createApp({
+  // Vue can intervene with html structure by using template
+    template: `
+    <h2>This is from app2 template: {{ hello }}</h2>
+  `,
+    data() {
+        return {
+            hello: "HelloWorld!",
+        };
+    },
+});
+
+app.mount("#app");
+app2.mount("#app2");
