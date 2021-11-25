836be86e8dca968836b548d4e39357e1b881174e
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-25 16:58:20 +0700

    add using vue cli

---
+++ 06_vue_cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="detailToggle">
+            {{ detailVisible ? "Hide" : "Show" }} Details
+        </button>
+        <ul v-if="detailVisible">
+            <li><strong>Phone: </strong> {{ friend.phone }}</li>
+            <li><strong>e-mail: </strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailVisible: false,
+            friend: {
+                id: "manuel",
+                name: "Manuel",
+                phone: "0123 45678 90",
+                email: "manuel@mail.com",
+            },
+        };
+    },
+    methods: {
+        detailToggle() {
+            this.detailVisible = !this.detailVisible;
+        },
+    },
+};
+</script>
---
+++ 06_vue_cli/vue-cli-01-a-new-vue-project/src/main.js
@@ -0,0 +1,11 @@
+// Import from package require only package name
+import { createApp } from "vue";
+
+// Import file need to input direction
+import App from "./App.vue";
+import FriendContact from "./components/FriendContact.vue";
+
+const app = createApp(App);
+app.mount("#app");
+
+app.component("friend-contact", FriendContact)
